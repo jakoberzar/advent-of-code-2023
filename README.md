7f7c3eb26b988e68678ec58e9e5f2d207b97ee31
Jakob Erzar <jakob.erzar@gmail.com>
2023-12-01 21:55:01 +0100

    Add solutions for day 1.

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
-# advent-of-code-2023
+# Advent of Code 2023
+
+My solutions for Advent of Code 2023. This year I am learning Zig. I might also do some OCaml if I feel like it.
+
+| Day | Nickname  | Zig | OCaml |
+| --- | --------- | --- | ----- |
+| 1   | Trebuchet | ✔️  |       |
+| 2   |           |     |       |
+| 3   |           |     |       |
+| 4   |           |     |       |
+| 5   |           |     |       |
+| 6   |           |     |       |
+| 7   |           |     |       |
+| 8   |           |     |       |
+| 9   |           |     |       |
+| 10  |           |     |       |
+| 11  |           |     |       |
+| 12  |           |     |       |
+| 13  |           |     |       |
+| 14  |           |     |       |
+| 15  |           |     |       |
+| 16  |           |     |       |
+| 17  |           |     |       |
+| 18  |           |     |       |
+| 19  |           |     |       |
+| 20  |           |     |       |
+| 21  |           |     |       |
+| 22  |           |     |       |
+| 23  |           |     |       |
+| 24  |           |     |       |
+| 25  |           |     |       |
+
+## Script for starting a new day
+
+```bash
+./new_day.sh <day_name>
+```
+
+## Zig
+
+### Setting up
+
+This was compiled with Zig version `0.12.0-dev.1768+39a966b0a`.
+
+Due to @embedFile restrictions, create a symlink to the inputs folder: `ln -s $PWD/inputs zig/input`
+
+### Running code
+
+For development
+`zig run zig/day01.zig`
+
+For optimized build
+
+```bash
+zig build-exe -O ReleaseFast -femit-bin=build/day01 zig/day01.zig
+# Benchmark
+hyperfine -w 3 -r 10 -N build/day01
+```
+
+### Testing code
+
+`zig test zig/day01.zig`
+
+## Other language candidates
+
+- OCaml for some practice
+- Go to see what not caring about code style feels like
+- Kotlin for some more Android practice
+- Rust to be happy and refresh knowledge
+- Dart to see what it's like and learn Flutter
---
+++ zig/day01.zig
@@ -0,0 +1,117 @@
+const std = @import("std");
+const expect = std.testing.expect;
+
+const day = "day-01";
+const simple = @embedFile("./inputs/" ++ day ++ "/simple.txt");
+const simple2 = @embedFile("./inputs/" ++ day ++ "/simple2.txt");
+const full = @embedFile("./inputs/" ++ day ++ "/full.txt");
+
+fn charToDigit(char: u8) u8 {
+    return char - '0';
+}
+
+fn tryDecodeDigit(s: []const u8, allow_strings: bool) ?u32 {
+    if (std.ascii.isDigit(s[0])) {
+        return charToDigit(s[0]);
+    }
+
+    if (!allow_strings or s.len < 3) {
+        // Every number needs at least 3 characters
+        return null;
+    }
+    if (std.mem.startsWith(u8, s, "one")) {
+        return 1;
+    } else if (std.mem.startsWith(u8, s, "two")) {
+        return 2;
+    } else if (std.mem.startsWith(u8, s, "three")) {
+        return 3;
+    } else if (std.mem.startsWith(u8, s, "four")) {
+        return 4;
+    } else if (std.mem.startsWith(u8, s, "five")) {
+        return 5;
+    } else if (std.mem.startsWith(u8, s, "six")) {
+        return 6;
+    } else if (std.mem.startsWith(u8, s, "seven")) {
+        return 7;
+    } else if (std.mem.startsWith(u8, s, "eight")) {
+        return 8;
+    } else if (std.mem.startsWith(u8, s, "nine")) {
+        return 9;
+    }
+
+    return null;
+}
+
+fn findCalibrationValue(line: []const u8, allow_digit_strings: bool) !u32 {
+    var first_number_or_null: ?u32 = null;
+    for (line, 0..) |_, idx| {
+        const digit_or_null = tryDecodeDigit(line[idx..], allow_digit_strings);
+        if (digit_or_null != null) {
+            first_number_or_null = digit_or_null.?;
+            break;
+        }
+    }
+    if (first_number_or_null == null) {
+        return error.NoDigitInLine;
+    }
+    const first_number = first_number_or_null.?;
+
+    var last_number: u32 = undefined;
+    var idx = line.len;
+    while (idx >= 0) {
+        idx -= 1;
+        const digit_or_null = tryDecodeDigit(line[idx..], allow_digit_strings);
+        if (digit_or_null != null) {
+            last_number = digit_or_null.?;
+            break;
+        }
+    }
+    return first_number * 10 + last_number;
+}
+
+fn solve(input: [:0]const u8, allow_digit_strings: bool) !u64 {
+    var lines_iterator = std.mem.splitScalar(u8, input, '\n');
+    var sum: u64 = 0;
+    while (lines_iterator.peek() != null) {
+        const line = lines_iterator.next().?;
+        if (line.len == 0) continue;
+        const value = try findCalibrationValue(line, allow_digit_strings);
+        sum += value;
+    }
+    return sum;
+}
+
+pub fn solve_star1(input: [:0]const u8) !u64 {
+    return solve(input, false);
+}
+
+pub fn solve_star2(input: [:0]const u8) !u64 {
+    return solve(input, true);
+}
+
+pub fn main() !void {
+    const result1 = try solve_star1(full);
+    std.debug.print("Star 1 result is {}\n", .{result1});
+    const result2 = try solve_star2(full);
+    std.debug.print("Star 2 result is {}\n", .{result2});
+}
+
+test "star 1 simple" {
+    const result = try solve_star1(simple);
+    try expect(result == 142);
+}
+
+test "star 1 full" {
+    const result = try solve_star1(full);
+    try expect(result == 55971);
+}
+
+test "star 2 simple" {
+    const result = try solve_star2(simple2);
+    try expect(result == 281);
+}
+
+test "star 2 full" {
+    const result = try solve_star2(full);
+    try expect(result == 54719);
+}
