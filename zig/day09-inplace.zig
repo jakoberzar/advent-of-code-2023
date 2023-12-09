9da2e640c3ac990f447dcc5690b3d67a3417ba89
Jakob Erzar <jakob.erzar@gmail.com>
2023-12-09 16:32:19 +0100

    Add optimized solutions for day 9.

--- zig/day09-inplace.zig
+++ zig/day09-inplace.zig
@@ -22,7 +22,7 @@ fn parseInput(allocator: std.mem.Allocator, input: [:0]const u8, out_sequences: 
         next_line = lines_iterator.next();
 
         try out_sequences.append(NumSeq.init(allocator));
-        var seq: *NumSeq = &out_sequences.items[out_sequences.items.len - 1];
+        const seq: *NumSeq = &out_sequences.items[out_sequences.items.len - 1];
         var num_iterator = std.mem.splitScalar(u8, line, ' ');
         var next_num = num_iterator.next();
         while (next_num != null) {
@@ -40,24 +40,22 @@ fn allValuesZero(sequence: *NumSeq) bool {
     return true;
 }
 
-fn createLowerLayer(allocator: std.mem.Allocator, sequence: *NumSeq) NumSeq {
-    var lower_seq = NumSeq.initCapacity(allocator, sequence.items.len - 1) catch unreachable;
+fn createLowerLayer(sequence: *NumSeq) void {
     for (sequence.items[0 .. sequence.items.len - 1], 0..) |n, idx| {
-        lower_seq.append(sequence.items[idx + 1] - n) catch unreachable;
+        sequence.items[idx] = sequence.items[idx + 1] - n;
     }
-    return lower_seq;
 }
 
 fn getNext(sequence: *NumSeq) i32 {
     if (allValuesZero(sequence)) return 0;
 
     // Create lower layer
-    var lower_seq = createLowerLayer(ga, sequence);
-    defer lower_seq.deinit();
+    createLowerLayer(sequence);
+    const last = sequence.pop();
     // Get value for next element
-    const child_next = getNext(&lower_seq);
-    const next = sequence.getLast() + child_next;
-    // std.debug.print("For slice [{} {}], child next is {} and next is {}\n", .{ sequence.items[0], sequence.items[1], child_next, next });
+    const child_next = getNext(sequence);
+    const next = last + child_next;
+    // std.debug.print("For slice ending {}, child next is {} and next is {}\n", .{ last, child_next, next });
     return next;
 }
 
@@ -65,11 +63,12 @@ fn getPrevious(sequence: *NumSeq) i32 {
     if (allValuesZero(sequence)) return 0;
 
     // Create lower layer
-    var lower_seq = createLowerLayer(ga, sequence);
-    defer lower_seq.deinit();
+    const first = sequence.items[0];
+    createLowerLayer(sequence);
+    _ = sequence.pop();
     // Get value for previous element
-    const child_previous = getPrevious(&lower_seq);
-    const previous = sequence.items[0] - child_previous;
+    const child_previous = getPrevious(sequence);
+    const previous = first - child_previous;
     // std.debug.print("For slice [{} {}], child previous is {} and previous is {}\n", .{ sequence.items[0], sequence.items[1], child_previous, previous });
     return previous;
 }
@@ -92,32 +91,43 @@ pub fn solveStar2(sequences: *ArrayList(NumSeq)) i64 {
     return sum;
 }
 
+fn copySequences(sequences: *ArrayList(NumSeq)) !ArrayList(NumSeq) {
+    const sequences_copy = try sequences.clone();
+    for (sequences_copy.items, 0..) |_, idx| {
+        sequences_copy.items[idx] = try sequences.items[idx].clone();
+    }
+    return sequences_copy;
+}
+
 pub fn main() !void {
     var sequences = ArrayList(NumSeq).init(ga);
     try parseInput(ga, full, &sequences);
+    var sequences_copy = try copySequences(&sequences);
 
     const result1 = solveStar1(&sequences);
     std.debug.print("Star 1 result is {}\n", .{result1});
-    const result2 = solveStar2(&sequences);
+    const result2 = solveStar2(&sequences_copy);
     std.debug.print("Star 2 result is {}\n", .{result2});
 }
 
 test "simple" {
     var sequences = ArrayList(NumSeq).init(ga);
     try parseInput(ga, simple, &sequences);
+    var sequences_copy = try copySequences(&sequences);
 
     const result1 = solveStar1(&sequences);
     try expect(result1 == 114);
-    const result2 = solveStar2(&sequences);
+    const result2 = solveStar2(&sequences_copy);
     try expect(result2 == 2);
 }
 
 test "full" {
     var sequences = ArrayList(NumSeq).init(ga);
     try parseInput(ga, full, &sequences);
+    var sequences_copy = try copySequences(&sequences);
 
     const result1 = solveStar1(&sequences);
     try expect(result1 == 1581679977);
-    const result2 = solveStar2(&sequences);
+    const result2 = solveStar2(&sequences_copy);
     try expect(result2 == 889);
 }
--- zig/day09-iterative.zig
+++ zig/day09-iterative.zig
@@ -22,7 +22,7 @@ fn parseInput(allocator: std.mem.Allocator, input: [:0]const u8, out_sequences: 
         next_line = lines_iterator.next();
 
         try out_sequences.append(NumSeq.init(allocator));
-        var seq: *NumSeq = &out_sequences.items[out_sequences.items.len - 1];
+        const seq: *NumSeq = &out_sequences.items[out_sequences.items.len - 1];
         var num_iterator = std.mem.splitScalar(u8, line, ' ');
         var next_num = num_iterator.next();
         while (next_num != null) {
@@ -40,84 +40,80 @@ fn allValuesZero(sequence: *NumSeq) bool {
     return true;
 }
 
-fn createLowerLayer(allocator: std.mem.Allocator, sequence: *NumSeq) NumSeq {
-    var lower_seq = NumSeq.initCapacity(allocator, sequence.items.len - 1) catch unreachable;
+fn createLowerLayer(sequence: *NumSeq) void {
     for (sequence.items[0 .. sequence.items.len - 1], 0..) |n, idx| {
-        lower_seq.append(sequence.items[idx + 1] - n) catch unreachable;
+        sequence.items[idx] = sequence.items[idx + 1] - n;
     }
-    return lower_seq;
-}
-
-fn getNext(sequence: *NumSeq) i32 {
-    if (allValuesZero(sequence)) return 0;
-
-    // Create lower layer
-    var lower_seq = createLowerLayer(ga, sequence);
-    defer lower_seq.deinit();
-    // Get value for next element
-    const child_next = getNext(&lower_seq);
-    const next = sequence.getLast() + child_next;
-    // std.debug.print("For slice [{} {}], child next is {} and next is {}\n", .{ sequence.items[0], sequence.items[1], child_next, next });
-    return next;
-}
-
-fn getPrevious(sequence: *NumSeq) i32 {
-    if (allValuesZero(sequence)) return 0;
-
-    // Create lower layer
-    var lower_seq = createLowerLayer(ga, sequence);
-    defer lower_seq.deinit();
-    // Get value for previous element
-    const child_previous = getPrevious(&lower_seq);
-    const previous = sequence.items[0] - child_previous;
-    // std.debug.print("For slice [{} {}], child previous is {} and previous is {}\n", .{ sequence.items[0], sequence.items[1], child_previous, previous });
-    return previous;
 }
 
 pub fn solveStar1(sequences: *ArrayList(NumSeq)) i64 {
     var sum: i64 = 0;
     for (sequences.items) |*sequence| {
-        const next = getNext(sequence);
-        sum += next;
+        while (!allValuesZero(sequence)) {
+            createLowerLayer(sequence);
+            sum += sequence.pop();
+        }
     }
     return sum;
 }
 
+// 16 - 3 = 13
+// 5 - 2 = 3
+// 4 - 2 = 2
+// 2 - 0 = 2
+// 16 - (5 - (4 - (2 - 0)))
+// 16 - 5 + 4 - 2 = 11 + 4 - 2 = 15 - 2 = 13
 pub fn solveStar2(sequences: *ArrayList(NumSeq)) i64 {
     var sum: i64 = 0;
     for (sequences.items) |*sequence| {
-        const previous = getPrevious(sequence);
-        sum += previous;
+        var sign: i32 = 1;
+        while (!allValuesZero(sequence)) {
+            sum += sign * sequence.items[0];
+            sign *= -1;
+            createLowerLayer(sequence);
+            _ = sequence.pop();
+        }
     }
     return sum;
+}
+
+fn copySequences(sequences: *ArrayList(NumSeq)) !ArrayList(NumSeq) {
+    const sequences_copy = try sequences.clone();
+    for (sequences_copy.items, 0..) |_, idx| {
+        sequences_copy.items[idx] = try sequences.items[idx].clone();
+    }
+    return sequences_copy;
 }
 
 pub fn main() !void {
     var sequences = ArrayList(NumSeq).init(ga);
     try parseInput(ga, full, &sequences);
+    var sequences_copy = try copySequences(&sequences);
 
     const result1 = solveStar1(&sequences);
     std.debug.print("Star 1 result is {}\n", .{result1});
-    const result2 = solveStar2(&sequences);
+    const result2 = solveStar2(&sequences_copy);
     std.debug.print("Star 2 result is {}\n", .{result2});
 }
 
 test "simple" {
     var sequences = ArrayList(NumSeq).init(ga);
     try parseInput(ga, simple, &sequences);
+    var sequences_copy = try copySequences(&sequences);
 
     const result1 = solveStar1(&sequences);
     try expect(result1 == 114);
-    const result2 = solveStar2(&sequences);
+    const result2 = solveStar2(&sequences_copy);
     try expect(result2 == 2);
 }
 
 test "full" {
     var sequences = ArrayList(NumSeq).init(ga);
     try parseInput(ga, full, &sequences);
+    var sequences_copy = try copySequences(&sequences);
 
     const result1 = solveStar1(&sequences);
     try expect(result1 == 1581679977);
-    const result2 = solveStar2(&sequences);
+    const result2 = solveStar2(&sequences_copy);
     try expect(result2 == 889);
 }
